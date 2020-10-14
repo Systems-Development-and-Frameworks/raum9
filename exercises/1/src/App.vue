b45fd72c9f2970f3e5563be63e816fa331651b7d
jannis <jannis.bernstein@twotickets.de>
2020-10-14 17:16:08 +0200

    erster Stand aufgabe 1

---
+++ exercises/1/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import NewsItem from './components/NewsItem'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    NewsItem
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
