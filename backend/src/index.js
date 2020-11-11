82b0b043d31900382715ef89c53fac447eaf1fbe
tobias <thinkdifferent055@gmail.com>
2020-11-11 17:05:31 +0100

    Add apollo-server-test, add graphql typeDefs

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,34 +1,77 @@
 const {ApolloServer, gql} = require('apollo-server');
+const { RESTDataSource } = require('apollo-datasource-rest');
 
 const typeDefs = gql`
-    type Book {
-        title: String
-        author: String
+    type Post {
+        id: ID!
+        title: String!
+        votes: Int!
+        author: User!
+    }
+
+    type User {
+        name: ID!
+        posts: [Post]
     }
 
     type Query {
-        books: [Book]
+        posts: [Post]
+        users: [User]
+    }
+
+    type Mutation {
+        write(post: PostInput!): Post
+
+        # ⚠️ FIXME in exercise #4
+        # mock voter until we have authentication
+        upvote(id: ID!, voter: UserInput!): Post
+    }
+
+    input PostInput {
+        title: String!
+
+        # ⚠️ FIXME in exercise #4
+        # mock author until we have authentication
+        author: UserInput!
+    }
+
+    input UserInput {
+        name: String!
     }
 `;
 
-const books = [
-    {
-        title: 'The Awakening',
-        author: 'Kate Chopin'
-    },
-    {
-        title: 'City of Glass',
-        author: 'Paul Auster'
+class PostDataStore extends RESTDataSource {
+
+    constructor() {
+        super();
+
+        this.posts = [
+            {
+                id: 1,
+                title: "Start Message",
+                votes: 1
+            }
+        ]
     }
-];
+
+    async getPosts() {
+        return this.posts;
+    }
+}
 
 const resolvers = {
     Query: {
-        books: () => books,
+        posts: (parent, args, {dataSources}) => dataSources.postsDataStore.getPosts(),
     }
 };
 
-const server = new ApolloServer({typeDefs, resolvers});
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources: () => ({
+        postsDataStore: new PostDataStore()
+    })
+});
 
 // The `listen` method launches a web server.
 server.listen().then(({url}) => {
