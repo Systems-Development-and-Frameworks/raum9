49bc3484441d01683c662ef47d21cc6c3992d5d0
tobias <thinkdifferent055@gmail.com>
2020-11-25 20:53:44 +0100

    Fix linting issues

--- backend/src/Permissions.js
+++ backend/src/Permissions.js
@@ -4,7 +4,7 @@ const isAuthenticated = rule({cache: 'contextual'})(
     async (parent, args, ctx, info) => {
         const token = ctx.user;
         if (token) {
-            const user = ctx.dataSources.userDataStore.getUserById(token.uid)
+            const user = ctx.dataSources.userDataStore.getUserById(token.uid);
             if (user) {
                 return true;
             }
