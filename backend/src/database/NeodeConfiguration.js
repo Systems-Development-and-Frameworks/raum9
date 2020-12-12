64f4bbd4338a0aaa7d820b5bdd11ce26dd7e750b
tobias <thinkdifferent055@gmail.com>
2020-12-12 14:42:47 +0100

    Cleanup

--- backend/src/database/NeodeConfiguration.js
+++ backend/src/database/NeodeConfiguration.js
@@ -1,6 +1,5 @@
 const Neode = require('neode');
 
-const dir = `${__dirname}/model`;
-// eslint-disable-next-line new-cap
-const instance = new Neode(process.env.NEO4J_URL, process.env.NEO4J_USER, process.env.NEO4J_PASSWORD).withDirectory(dir);
+const instance = new Neode(process.env.NEO4J_URL, process.env.NEO4J_USER, process.env.NEO4J_PASSWORD)
+    .withDirectory(`${__dirname}/model`);
 module.exports = instance;
