1372bb57c0441ed1aadfab7b9e5bac4e5e80eec9
tobias <thinkdifferent055@gmail.com>
2021-01-13 17:13:16 +0100

    Add logout button

--- webapp/components/Menu.vue
+++ webapp/components/Menu.vue
@@ -1,14 +1,20 @@
 <template>
-  <nuxt-link to="/login" v-if="!loggedIn">Login({{ loggedIn }})</nuxt-link>
+  <div>
+    <nuxt-link to="/login" v-if="!loggedIn">Login</nuxt-link>
+    <button @click="logout" v-if="loggedIn">Logout</button>
+  </div>
 </template>
 
 <script>
-import {mapGetters} from "vuex";
+import {mapActions, mapGetters} from "vuex";
 
 export default {
   name: "LoginForm",
   computed: {
     ...mapGetters('auth', ['loggedIn'])
+  },
+  methods: {
+    ...mapActions('auth', ['logout'])
   }
 };
 </script>
