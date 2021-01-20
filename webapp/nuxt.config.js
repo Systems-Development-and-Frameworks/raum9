f935a06f294fffa0efdeb9aca2710c0d0116a6f8
tobias <thinkdifferent055@gmail.com>
2021-01-20 18:52:34 +0100

    Try to add theme color for PWA

--- webapp/nuxt.config.js
+++ webapp/nuxt.config.js
@@ -1,11 +1,13 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'nuxt',
+    title: 'HackerNews',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'theme-color', content: '#2196F3'},
+      { name: 'theme_color', content: '#2196F3'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
